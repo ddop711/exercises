f8451ab1ba0f0fb82b0ed328f9afd2910ebe2697
ddop711 <ddop711@gmail.com>
2026-08-21 05:48:39 +0200

    Initialize attempt counter and update messages in Gæt et tal game

--- gaetettal_omvendt/script.js
+++ gaetettal_omvendt/script.js
@@ -1,19 +1,23 @@
 let min = 0;
 let max = 101;
 let gaet = 50;
+let forsog = 0;
 
 let start = document.getElementById("start");
 let lav = document.getElementById("lav");
 let rigtigt = document.getElementById("rigtigt");
 let hoej = document.getElementById("hoej");
 let tekst = document.getElementById("gaet");
+let besked = document.getElementById("besked");
 
 start.onclick = function () {
   min = 0;
   max = 101;
   gaet = 50;
+  forsog = 1;
 
   tekst.innerHTML = gaet;
+  besked.innerHTML = "Forsøg: " + forsog;
 
   start.disabled = true;
   lav.disabled = false;
@@ -24,19 +28,24 @@ start.onclick = function () {
 lav.onclick = function () {
   min = gaet;
   gaet = Math.floor((min + max) / 2);
+  forsog++;
 
   tekst.innerHTML = gaet;
+  besked.innerHTML = "Forsøg: " + forsog;
 };
 
 hoej.onclick = function () {
   max = gaet;
   gaet = Math.floor((min + max) / 2);
+  forsog++;
 
   tekst.innerHTML = gaet;
+  besked.innerHTML = "Forsøg: " + forsog;
 };
 
 rigtigt.onclick = function () {
-  tekst.innerHTML = "YAY";
+  tekst.innerHTML = "YAY!";
+  besked.innerHTML = "Jeg brugte " + forsog + " forsøg.";
 
   lav.disabled = true;
   rigtigt.disabled = true;
