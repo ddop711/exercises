61717c42d7662dda5d9d24ad3196c10cfc6f0462
ddop711 <ddop711@gmail.com>
2026-08-21 05:39:13 +0200

    Add initial implementation of Gæt et tal game with HTML, CSS, and JavaScript

---
+++ gaetettal_omvendt/script.js
@@ -0,0 +1,45 @@
+let min = 0;
+let max = 101;
+let gaet = 50;
+
+let start = document.getElementById("start");
+let lav = document.getElementById("lav");
+let rigtigt = document.getElementById("rigtigt");
+let hoej = document.getElementById("hoej");
+let tekst = document.getElementById("gaet");
+
+start.onclick = function () {
+  min = 0;
+  max = 101;
+  gaet = 50;
+
+  tekst.innerHTML = gaet;
+
+  start.disabled = true;
+  lav.disabled = false;
+  rigtigt.disabled = false;
+  hoej.disabled = false;
+};
+
+lav.onclick = function () {
+  min = gaet;
+  gaet = Math.floor((min + max) / 2);
+
+  tekst.innerHTML = gaet;
+};
+
+hoej.onclick = function () {
+  max = gaet;
+  gaet = Math.floor((min + max) / 2);
+
+  tekst.innerHTML = gaet;
+};
+
+rigtigt.onclick = function () {
+  tekst.innerHTML = "YAY " + gaet;
+
+  lav.disabled = true;
+  rigtigt.disabled = true;
+  hoej.disabled = true;
+  start.disabled = false;
+};
